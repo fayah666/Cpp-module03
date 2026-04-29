fe3d4856eaa50b538790def9d396e92843859e22
hfandres <hfandres@student.42antananarivo.mg>
2026-04-29 13:14:18 +0300

    Implement ClapTrap class with basic functionality and main program for testing

---
+++ ClapTrap.cpp
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfandres <hfandres@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/29 11:05:26 by hfandres          #+#    #+#             */
+/*   Updated: 2026/04/29 13:12:24 by hfandres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void) :
+	_hitPoints(10), _energyPoints(10), _attackDamage(0) {
+	std::cout << COLOR_YELLOW << _name << ":" << COLOR_RESET << std::endl;
+	std::cout << COLOR_BLUE << "Spawning with style." << COLOR_RESET << std::endl;
+}
+ClapTrap::~ClapTrap() {
+	std::cout << COLOR_YELLOW << _name << ":" << COLOR_RESET << std::endl;
+	std::cout << COLOR_RED << "So this is how it ends" << COLOR_RESET << std::endl;
+}
+ClapTrap::ClapTrap(const std::string& name) :
+	_name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+	std::cout << COLOR_YELLOW << _name << ":" << COLOR_RESET << std::endl;
+	std::cout << COLOR_BLUE << "Showtime" << COLOR_RESET << std::endl;
+}
+ClapTrap::ClapTrap(const ClapTrap& other) {
+	if (this != &other)
+		*this = other;
+}
+ClapTrap&	ClapTrap::operator=(const ClapTrap& other) {
+	if (this == &other)
+		return (*this);
+	_name = other._name;
+	_hitPoints = other._hitPoints;
+	_energyPoints = other._energyPoints;
+	_attackDamage = other._attackDamage;
+	return (*this);
+}
+std::string	ClapTrap::getName(void) const {
+	return (this->_name);
+}
+int	ClapTrap::getHitPoints(void) const {
+	return (this->_hitPoints);
+}
+int	ClapTrap::getEnergyPoints(void) const {
+	return (this->_energyPoints);
+}
+int	ClapTrap::getAttackDamage(void) const {
+	return (this->_attackDamage);
+}
+void	ClapTrap::setName(const std::string& name) {
+	this->_name = name;
+}
+void	ClapTrap::setHitPoints(const int hitpoints) {
+	this->_hitPoints = hitpoints;
+}
+void	ClapTrap::setEnergyPoints(const int energyPoints) {
+	this->_energyPoints = energyPoints;
+}
+void	ClapTrap::setAttackDamage(const int attackDamage) {
+	this->_attackDamage = attackDamage;
+}
+void	ClapTrap::attack(const std::string& target) {
+	std::cout << COLOR_YELLOW << _name << ":" << COLOR_RESET << std::endl;
+	std::cout << COLOR_BLUE << "Swing first, think later!" << COLOR_RESET << std::endl;
+	_energyPoints -= 1;
+	if (_energyPoints <= 0 || _hitPoints <= 0)
+	{
+		std::cout << COLOR_RED << "Battery low. Like, really low." << COLOR_RESET << std::endl;
+		_energyPoints = 0;
+		return ;
+	}
+	std::cout << "Claptrap " << _name << " attacks " << target << std::endl;
+	std::cout << "causing " << _attackDamage <<  " points of damage!" << std::endl;
+}
+void	ClapTrap::takeDamage(unsigned int amount) {
+	std::cout << COLOR_YELLOW << _name << ":" << COLOR_RESET << std::endl;
+	std::cout << COLOR_RED << "I’m fine. Totally fine. Probably." << COLOR_RESET << std::endl;
+	std::cout << "Claptrap " << _name << " takes " << amount << " points of damage!" << std::endl;
+	_hitPoints -= amount;
+	if (_hitPoints <= 0)
+		_hitPoints = 0;
+}
+void	ClapTrap::beRepaired(unsigned int amount) {
+	std::cout << COLOR_YELLOW << _name << ":" << COLOR_RESET << std::endl;
+	std::cout << COLOR_GREEN << "Magic fixes everything." << COLOR_RESET << std::endl;
+	_energyPoints -= 1;
+	if (_energyPoints <= 0 || _hitPoints <= 0)
+	{
+		std::cout << COLOR_RED << "Battery low. Like, really low." << COLOR_RESET << std::endl;
+		_energyPoints = 0;
+		return ;
+	}
+	std::cout << "Claptrap " << _name << " repairs itself for " << amount << " hit points!" << std::endl;
+	_hitPoints += amount;
+}
---
+++ main.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfandres <hfandres@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/29 11:25:39 by hfandres          #+#    #+#             */
+/*   Updated: 2026/04/29 13:10:27 by hfandres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+	ClapTrap alpha("Alpha");
+	ClapTrap beta("Beta");
+	std::cout << "\n--- Initial Stats ---\n";
+	std::cout << alpha.getName() << " HP: " << alpha.getHitPoints()
+			<< " EP: " << alpha.getEnergyPoints() << std::endl;
+	std::cout << beta.getName() << " HP: " << beta.getHitPoints()
+			<< " EP: " << beta.getEnergyPoints() << std::endl;
+	alpha.setAttackDamage(6);
+	beta.setAttackDamage(4);
+	std::cout << "\n--- Fight Start ---\n";
+	for (int turn = 1; turn <= 2; turn++)
+	{
+		std::cout << "\n[TURN " << turn << "]\n";
+		if (alpha.getEnergyPoints() > 0 && alpha.getHitPoints() > 0)
+		{
+			alpha.attack(beta.getName());
+			beta.takeDamage(alpha.getAttackDamage());
+		}
+		if (beta.getHitPoints() > 0)
+		{
+			if (beta.getHitPoints() < 5 && beta.getEnergyPoints() > 0)
+				beta.beRepaired(3);
+			else if (beta.getEnergyPoints() > 0)
+			{
+				beta.attack(alpha.getName());
+				alpha.takeDamage(beta.getAttackDamage());
+			}
+		}
+		if (alpha.getHitPoints() <= 0 || beta.getHitPoints() <= 0)
+			break;
+	}
+	std::cout << "\n--- Copy Constructor Test ---\n";
+	ClapTrap clone(alpha);
+	std::cout << "Clone of " << alpha.getName()
+			<< " HP: " << clone.getHitPoints() << std::endl;
+	std::cout << "\n--- Assignment Operator Test ---\n";
+	clone = beta;
+	std::cout << "Clone now copied from " << beta.getName()
+			<< " HP: " << clone.getHitPoints() << std::endl;
+	std::cout << "\n--- Edge Case Test ---\n";
+	alpha.setHitPoints(0);
+	alpha.attack("Ghost");
+	alpha.beRepaired(5);
+	std::cout << "\n--- Final Stats ---\n";
+	std::cout << alpha.getName() << " HP: " << alpha.getHitPoints() << std::endl;
+	std::cout << beta.getName() << " HP: " << beta.getHitPoints() << std::endl;
+	return 0;
+}
